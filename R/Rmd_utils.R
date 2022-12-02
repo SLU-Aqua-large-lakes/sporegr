4aa11da67b3b296da0364c0f88e59d2d9d6f4862
Anders Kagervall <Anders.Kagervall@slu.se>
2022-12-02 14:53:53 +0100

    Add Rmd_utils.R and move integer_breaks() to this file

---
+++ R/Rmd_utils.R
@@ -0,0 +1,44 @@
+# Collection of functions that can be useful to make nicer Rmd documents.
+
+
+# function for nicer plots
+#' integer_breaks
+#'
+#' Put breaks only om integer values
+#'
+#' @param n Number of wanted breaks
+#' @param ... passed to scales::pretty_breaks
+#'
+#' @return
+#' a function
+#' @export
+#'
+integer_breaks <- function(n = 5, ...) { # Helper function to get nicer scales
+  breaker <- scales::breaks_extended(n, ...)
+  function(x) {
+    breaks <- breaker(x)
+    breaks[breaks == floor(breaks)]
+  }
+}
+
+#' calc_date_breaks
+#'
+#' Helper function to help calculate the number of breaks needed an a date axis.
+#' The output is a string in the format "N days" suitable as value for parameter
+#' date_breaks in scale_x_date() and friends.
+#'
+#' @param first_date a date or string that as.Date can convert to date
+#' @param last_date a date or string that as.Date can convert to date
+#'
+#' @return
+#' A character string
+#' @export
+#'
+#' @examples
+#' calc_date_breaks("2022-06-01", "2022-06-18")
+calc_date_breaks <- function(first_date, last_date) {
+  days_between <- floor(as.integer(
+    diff(range(c(as.Date(first_date), as.Date(last_date))))) / 15
+  )
+  return(paste0(days_between, " days"))
+}
